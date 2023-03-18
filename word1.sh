d1f185d909569e6b668b6844c803a8495a70e0b7
sowmyav3 <sowmya26sowmya@gmail.com>
2023-03-19 01:20:27 +0530

    review2

---
+++ word1.sh
@@ -0,0 +1,35 @@
+read -p "Enter a single digit number" n
+if (( $n==0 ))
+then
+	echo "Zero"
+elif (( $n==1 ))
+then
+	echo "One"
+elif (( $n==2 ))
+then
+        echo "Two"
+elif (( $n==3 ))
+then
+        echo "Three"
+elif (( $n==4 ))
+then
+        echo "Four"
+elif (( $n==5 ))
+then
+        echo "Five"
+elif (( $n==6 ))
+then
+        echo "Six"
+elif (( $n==7 ))
+then
+        echo "Seven"
+elif (( $n==8 ))
+then
+        echo "Eight"
+elif (( $n==9 ))
+then
+        echo "Nine"
+else
+	echo "Enter a valid number"
+fi
+
